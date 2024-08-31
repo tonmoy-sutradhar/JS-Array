ed5e84d9cd157867adf21d9f625a6901cb8b5bf1
Tonmoy <tonmoysutradhar2002@gmail.com>
2024-08-31 23:08:45 +0600

    Push-pop and shift-unshift concept added.

---
+++ push-pop.js
@@ -0,0 +1,25 @@
+// Array push
+// Push use korle value last theke remove hoi---->
+const numbers = [10, 7, 4, 5, 3, 1];
+
+console.log(numbers);
+numbers.push(100);
+numbers.push(500);
+numbers.push(30);
+numbers.push(25, 38, 11, 97);
+console.log(numbers);
+
+// ----------------------------------------------------->
+// Array pop
+// pop use korle value last theke remove hoi---->
+const friends = ["Tonmoy", "Sagor", "Rahim", "Karim", "Faisal"];
+console.log(friends);
+friends.pop();
+console.log(friends);
+friends.pop();
+console.log(friends);
+
+// Pop kora man gulo k variable rakha jai.....
+const outElement = friends.pop();
+console.log(outElement);
+console.log(friends);
---
+++ shift-unshift.js
@@ -0,0 +1,9 @@
+const friends = ["Tonmoy", "Sagor", "Rahim", "Karim", "Faisal"];
+
+// Shift use korle value 1st theke remove hoi---->
+friends.shift();
+console.log(friends);
+
+// Unshift------>
+friends.unshift("Meghla");
+console.log(friends);
