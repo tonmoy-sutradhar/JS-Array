565c4299ed41ae922db363c092c849a2a1393da5
Tonmoy <tonmoysutradhar2002@gmail.com>
2024-09-01 00:24:38 +0600

    Concat in 2 array.

---
+++ concateArray.js
@@ -0,0 +1,12 @@
+// Concat array
+
+const friends = ["Tonmoy", "Sagor", "Rahim", "Karim", "Faisal"];
+const numbers = [10, 7, 4, 5, 3, 1];
+
+console.log(friends);
+// Concat use kora hoi 2 ba akadik array k marge korar jonno
+console.log(friends.concat(numbers));
+
+// Slice use kora hoi array vag korar jonno(half);
+// index 1 theke suru hbe index 3 er agh porjonto
+console.log(friends.slice(1, 3));
